8aa4afe9613dc5fc69773e2b456a3c01d63b2479
Sharma <rahul.s.sharma@accenture.com>
2018-07-10 17:03:18 +0530

    Second Update

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ const API_KEY = 'f99f4908c4e3c3aae89549e337e241bd';
 const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 export  function fetchWeather(city) {
-    const url = `${ROOT_URL}&q=${city},us`;
+    const url = `${ROOT_URL}&q=${city},in`;
    
     const request = axios.get(url);
     
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
+import  GoogleMap  from '../components/google_map';
+
+
 class WeatherList extends Component {
+    renderWeather(cityData) {
+        const name = cityData.city.name;
+        const temps = cityData.list.map(weather=>weather.main.temp);
+        const pressures = cityData.list.map(weather=>weather.main.pressure)
+        const humidities = cityData.list.map(weather=>weather.main.humidity);
+        const {lon,lat} = cityData.city.coord;
+        //console.log(temps);
+        return (
+            <tr key={cityData.city.id}>
+                <td>
+                    <GoogleMap lon={lon} lat={lat}/>
+                    {name}
+                </td>
+                <td>
+                    <Chart color="green" data={pressures} unit="K"/>
+                </td>
+                <td>
+                    <Chart color="black" data={humidities} unit="hPa"/>
+                </td>
+                <td>
+                    <Chart color="orange" data={temps} unit="%"/>
+                </td>
+            </tr>
+        )
+    }
     render() {
         return (
             <table className='table'>
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr></tr>
+                    {this.props.weather.map(this.renderWeather)}
                 </tbody>
             </table>
         )
